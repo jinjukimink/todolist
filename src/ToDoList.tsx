4fe6cda0168928a44220bbbc99d03646ee499850
jinjukim_web <63224644+jinjukimink@users.noreply.github.com>
2024-02-29 15:23:47 +0900

    new branch

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,58 +1,57 @@
-import React from "react";
 import {useForm} from "react-hook-form";
 
-/*function ToDoList(){
-    const [toDo,setToDo]=useState("");
-    const [toDoError,setToDoError]=useState("");
-    const onChange=(event:React.FormEvent<HTMLInputElement>)=>{
-        const {currentTarget: {value},
-         }=event;
-         setToDoError("");
-         setToDo(value);
-         
-    }
-    const onSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
-        event.preventDefault();
-        console.log(toDo);
-        if(toDo.length<10){
-            return setToDoError("to do should be longer")
-        }
-        console.log("submit");
-    } 
+
+interface IForm{
+    toDo:string;
+
+}
+function ToDoList(){
+ 
+const{register,handleSubmit,setValue } = useForm<IForm>();
+
+const onSubmit=(data:IForm)=>{
+    console.log("add to do",data.toDo);
+    setValue("toDo","");
+}
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input value={toDo} onChange={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onChange} placeholder="write a to do"/>
-                <button>Add</button>
-                {toDoError!==""?toDoError:null}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("toDo",{
+                    required:"Please write to do.",   
+                })} placeholder="write a to do"/>
+                <button>Add</button> 
             </form> 
         </div>
     );
-}*/
-interface IFrom{
-    Email:"string";
+}
+export default ToDoList;
+
+/*interface IFrom{
+    Email:"string"; 
     firstName:"string";
     lastName:"string";
     userName:"string";
     password:"string";
     password_confirmation:"string";
+    extraError?:string;
 }
 
 function ToDoList(){
 
-    const { register,handleSubmit ,formState:{errors}} = useForm<IFrom>({
+    const { register,handleSubmit ,formState:{errors}, setError} = useForm<IFrom>({
         defaultValues:{
-    
-            
         }
     });
 
-    const onValid=(data:any)=>{
+    const onValid=(data:any)=>{//데이터가 유효할 때만 받는 함수
         //console.log(data);
-    }
-    // console.log(watch());
-    console.log(errors);//심지어 에러의 종류도 알려줌..
+        if(data.password !== data.password_confirmation){
+            setError("password_confirmation",{message:"Password are not the same"},{shouldFocus:true})//password_confimation에 오류가 있다면, 커서가 그쪽에 가게 한다.
+        }
+        setError("extraError",{message:"Server offline"});
+    };
+    console.log(errors);//심지어 에러의 종류도 알려줌.. 
 
 
     return <div>  
@@ -65,7 +64,11 @@ function ToDoList(){
                 })} placeholder="Email"/>
             <span>{errors.Email?.message}</span>
 
-            <input {...register("firstName",{required:"firstName is required"})} placeholder="firstName"/>
+            <input {...register("firstName",{required:"firstName is required",validate:{
+                noJinju: (value)=> value.includes("jinju")?"no jinjus allowed":true,
+                noNicos: (value)=> value.includes("nico")?"no nico allowed":true,
+            }
+            })} placeholder="firstName"/>
             <span>{errors.firstName?.message }</span>
 
             <input {...register("lastName",{ required:"lastName is required" , minLength:{
@@ -85,7 +88,8 @@ function ToDoList(){
             <input {...register("password_confirmation",{required:true, minLength:5 })} placeholder="password_confirmation"/>
             <span>{errors.password_confirmation?.message }</span>
             <button>Add</button>
+            <span>{errors?.extraError?.message}</span>
         </form>
     </div>
 }
-export default ToDoList;
+export default ToDoList;*/
