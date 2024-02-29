9ef0770c0cb499a06dd2a87c990dc53b2b2fb46d
jinjukim_web <63224644+jinjukimink@users.noreply.github.com>
2024-02-29 13:33:31 +0900

    에러메세지 to user

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import {Form, useForm} from "react-hook-form";
+import React from "react";
+import {useForm} from "react-hook-form";
 
 /*function ToDoList(){
     const [toDo,setToDo]=useState("");
@@ -23,36 +23,67 @@ import {Form, useForm} from "react-hook-form";
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input value={toDo} onChange={onChange} placeholder="write a to do"/>
+                <input value={toDo} onChange={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onChange} placeholder="write a to do"/>
                 <button>Add</button>
                 {toDoError!==""?toDoError:null}
             </form> 
         </div>
     );
 }*/
+interface IFrom{
+    Email:"string";
+    firstName:"string";
+    lastName:"string";
+    userName:"string";
+    password:"string";
+    password_confirmation:"string";
+}
 
 function ToDoList(){
 
-    const { register,handleSubmit ,formState} = useForm();
-    //console.log(register("Email"));//toDo라는 객체생성
+    const { register,handleSubmit ,formState:{errors}} = useForm<IFrom>({
+        defaultValues:{
+    
+            
+        }
+    });
+
     const onValid=(data:any)=>{
         //console.log(data);
     }
     // console.log(watch());
-    console.log(formState.errors);//심지어 에러의 종류도 알려줌..
+    console.log(errors);//심지어 에러의 종류도 알려줌..
 
-    return <div>
+
+    return <div>  
         <form style={{display:"flex", flexDirection: "column"}}
          onSubmit={handleSubmit(onValid)}>
-            <input {...register("Email",{required:true})} placeholder="Email"/>
-            <input {...register("firstName",{required:true})} placeholder="firstName"/>
-            <input {...register("lastName",{ required:true , minLength:{
+
+            <input {...register("Email",{ required:"Email is required", pattern:
+                {value:/^[A-Za-z0-9._%+-]+@naver\.com$/,
+                message:"Only naver.com emails allowed!!"}
+                })} placeholder="Email"/>
+            <span>{errors.Email?.message}</span>
+
+            <input {...register("firstName",{required:"firstName is required"})} placeholder="firstName"/>
+            <span>{errors.firstName?.message }</span>
+
+            <input {...register("lastName",{ required:"lastName is required" , minLength:{
                 value:5,
-                message:"Your password is too short",
+                message:"Your lastName is too short",
             } })} placeholder="lastName"/>
-            <input {...register("userName",{required:true})} placeholder="userName"/>
-            <input {...register("password",{required:"password is required", minLength:5})} placeholder="password"/>
+            <span>{errors.lastName?.message }</span>
+            <input {...register("userName",{required:"userName is requried"})} placeholder="userName"/>
+            <span>{errors.userName?.message }</span>
+
+            <input {...register("password",{required:"password is required", minLength:{
+                value:5,
+                message:"your password is too short"
+            }})} placeholder="password"/>
+            <span>{errors.password?.message }</span>
+
             <input {...register("password_confirmation",{required:true, minLength:5 })} placeholder="password_confirmation"/>
+            <span>{errors.password_confirmation?.message }</span>
             <button>Add</button>
         </form>
     </div>
